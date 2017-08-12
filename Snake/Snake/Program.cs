d0a0d11787d013b1f4f55c9d2350760dfb142f1e
hotBacon123 <nikaert243@gmail.com>
2017-08-12 21:24:33 +0400

    Function draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell world");
+            int x1 = 1, y1 = 3, x2 = 4, y2 = 5;
+            char sym1 = '*', sym2 = '*';
+            Draw(x1, y1, sym1);
+            Draw(x2, y2, sym2);
             Console.ReadLine();
         }
+
+        static void Draw(int x,int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
+
 }
